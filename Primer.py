a968dd4e25624b1f505252f7d6e4cf11887d18b1
AlbertoBalaguePadron <usr2276@salesianos-lacuesta.net>
2021-01-28 14:08:38 +0000

    Update Primer.py
    EStoy cambiando el fichero

--- Primer.py
+++ Primer.py
@@ -1,2 +1,10 @@
 # Primer fichero 
 print ("hola")
+
+letra = input ("dame una letra; ")
+
+print("Lo que escribo fue:" , letra )
+
+numero = float( input ("dame un número"))
+
+print("Lo que escribo fue:" , numero )
